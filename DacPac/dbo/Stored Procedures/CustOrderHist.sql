77add0199064eb59d97a4cd91d76ff8d976736f1
dvkr29 <reddy.dvkr@gmail.com>
2020-09-14 20:43:32 +0530

    commit changes

--- DacPac/dbo/Stored Procedures/CustOrderHist.sql
+++ DacPac/dbo/Stored Procedures/CustOrderHist.sql
@@ -1,6 +1,6 @@
 ﻿CREATE PROCEDURE [dbo].[CustOrderHist] @CustomerID nchar(5)
 AS
-SELECT ProductName --, Total=SUM(Quantity)
+SELECT ProductName,  Total=SUM(Quantity)
 FROM Products P, [Order Details] OD, Orders O, Customers C
 WHERE C.CustomerID = @CustomerID
 AND C.CustomerID = O.CustomerID AND O.OrderID = OD.OrderID AND OD.ProductID = P.ProductID
